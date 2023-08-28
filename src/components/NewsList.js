23ea813c373c5235e9779400d6f730a923bb2285
user1 <code.softwaredevg@gmail.com>
2023-08-28 23:32:47 +0100

    Commit v4

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -13,23 +13,23 @@ function NewsList() {
   }, []);
 
   return (
-    <div className="container mx-auto py-2 px-0 ">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="container mx-auto py-2 px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
         {articles.map((article) => (
           <Link to={`/article/${article.id}`} key={article.id}>
-            <div className="relative bg-[#747474] p-[1px] rounded shadow-md hover:shadow-lg">
+            <div className="relative  shadow-md hover:shadow-lg border-b">
               <img
                 src={article.image}
                 alt={article.title}
-                className="w-full h-60 max-w-full object-cover mb-0"
+                className="w-full h-64 max-w-full object-cover mb-0"
               />
               <div className="absolute inset-0 flex flex-col items-center">
-                <div className="bg-black bg-opacity-0 p-[1px] rounded text-center top-28 absolute inset-0 flex items-center justify-center">
-                  <h2 className="text-base font-poppins font-semibold bg-transparent/70 text-white px-3 py-1.5 text-left">
+                <div className="bg-black bg-opacity-0 p-[0px] rounded text-center top-[112px] absolute inset-0 flex items-center justify-center">
+                  <h2 className="w-full text-base font-poppins font-semibold bg-transparent/70 text-white px-3 py-1.5 text-left">
                     {article.title}
                   </h2>
                 </div>
-                <div className="bg-transparent/90 absolute bottom-1.5 left-0 w-auto px-0 py-1 rounded-e-full">
+                <div className="bg-transparent/90 absolute bottom-1.5 left-0 w-auto px-0 py-0.5 rounded-e-full">
                   {/* <p className="text-xs font-inter font-normal mb-1">
                     Date: {article.date}
                   </p> */}
