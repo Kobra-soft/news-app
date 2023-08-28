2e3ddb4398b307575d2eccd91da0861567dc8f62
user1 <code.softwaredevg@gmail.com>
2023-08-28 23:18:45 +0100

    Commit v4

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -13,7 +13,7 @@ function NewsList() {
   }, []);
 
   return (
-    <div className="container mx-auto p-1">
+    <div className="container mx-auto py-2 px-0 ">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {articles.map((article) => (
           <Link to={`/article/${article.id}`} key={article.id}>
@@ -21,20 +21,20 @@ function NewsList() {
               <img
                 src={article.image}
                 alt={article.title}
-                className="w-full h-64 max-w-full object-cover mb-0"
+                className="w-full h-60 max-w-full object-cover mb-0"
               />
               <div className="absolute inset-0 flex flex-col items-center">
-                <div className="bg-black bg-opacity-0 p-[1px] rounded text-center top-10 absolute inset-0 flex items-center justify-center">
-                  <h2 className="text-base font-inter font-black bg-transparent/60 text-white px-3">
+                <div className="bg-black bg-opacity-0 p-[1px] rounded text-center top-28 absolute inset-0 flex items-center justify-center">
+                  <h2 className="text-base font-poppins font-semibold bg-transparent/70 text-white px-3 py-1.5 text-left">
                     {article.title}
                   </h2>
                 </div>
-                <div className="bg-[#ebebebdd] text-white absolute bottom-2 left-2 w-auto px-2 py-1">
+                <div className="bg-transparent/90 absolute bottom-1.5 left-0 w-auto px-0 py-1 rounded-e-full">
                   {/* <p className="text-xs font-inter font-normal mb-1">
                     Date: {article.date}
                   </p> */}
-                  <p className="text-xs font-inter font-normal text-black">
-                    {article.author}
+                  <p className="text-xs font-poppins font-normal text-teal-400 px-3">
+                    {article.author} | {article.date}
                   </p>
                 </div>
               </div>
