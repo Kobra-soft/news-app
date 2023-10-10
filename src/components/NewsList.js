522d05a7ce32af3ffbfa24922dcbe63aa7b451a8
user1 <code.softwaredevg@gmail.com>
2023-10-10 17:07:44 +0100

    Commit - Website Cards / Container FIX

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -29,7 +29,7 @@ function NewsList({ isDarkMode }) {
     const apiUrl = `http://api.mediastack.com/v1/news?access_key=f6b499e9ef0d7e84bc4b01cd73323fb3&languages=en&countries=gb&limit=30`;
 
     // Fetch data from data2.json for the left side
-    axios.get("/data3.json").then((response) => {
+    axios.get("/data2.json").then((response) => {
       setLeftArticles(response.data);
     });
 
@@ -93,56 +93,82 @@ function NewsList({ isDarkMode }) {
       className="container mx-auto md:max-w-screen-xl
     pb-10 py-10 md:py-10 lg:py-10 xl:py-10 px-0 bg-gray-000"
     >
-      <div className="flex flex-col items-center justify-center bg-gray-300">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 bg-gray-200">
+      <div className="flex flex-col items-center justify-center bg-gray-000">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 bg-gray-000">
           {/* Left side */}
-          <div className="col-span-1 grid grid-cols-2 gap-3">
+          <div className="col-span-2">
             {leftArticles.map((article) => (
               <Link to={`/article/${article.id}`} key={article.id}>
                 {/* Your large news card content */}
                 <div
-                  className={`relative shadow-md hover:shadow-xl 
-                  rounded-t-xl rounded-b-xl ${
+                  style={{ alignItems: "center" }}
+                  className={` mb-3 relative shadow-md hover:shadow-lg rounded-t-xl rounded-b-xl flex ${
                     isDarkMode ? "bg-[#101010]" : "bg-white"
                   }`}
-                  style={{ maxWidth: "400px" }} // Adjust the width as needed
                 >
-                  {/* Big card content */}
-                  {/* Conditionally render the image or placeholder */}
-                  {article.image ? (
-                    <img
-                      src={article.image}
-                      alt=""
-                      className="w-full h-[225px] max-w-full object-center 
-                      sm:object-center md:object-center rounded-t-xl"
-                    />
-                  ) : (
-                    <img
-                      src={`http://via.placeholder.com/640x360`}
-                      alt=""
-                      className="w-full h-[225px] max-w-full object-center 
-                      sm:object-center md:object-center rounded-t-xl"
-                    />
-                  )}
+                  {/* Left side (Image) */}
+                  <div className="rounded-tl-xl rounded-bl-xl overflow-hidden h-[170px] w-[220px]">
+                    {article.image ? (
+                      <img
+                        src={article.image}
+                        alt=""
+                        className="w-full h-full object-cover"
+                      />
+                    ) : (
+                      <img
+                        src={`http://via.placeholder.com/150x225`}
+                        alt=""
+                        className="w-full h-full object-cover"
+                      />
+                    )}
+                  </div>
 
-                  {/* Rest of your large card content */}
-                  {/* ... */}
+                  {/* Right side (Content) */}
+                  <div className="flex-1 px-5 align-middle">
+                    {/* Source */}
+                    <p className="text-left text-xs text-gray-500">
+                      {article.source}
+                    </p>
+
+                    {/* Title */}
+                    <h2
+                      className="text-left text-xl mt-1 font-extrabold 
+                      transition-all duration-300 hover:text-red-500 leading-tight
+                      line-clamp-2"
+                      style={{
+                        maxWidth: "45ch",
+                        /* maxHeight: "2.5rem", */
+                        overflow: "hidden",
+                      }}
+                    >
+                      {article.title}
+                    </h2>
+
+                    {/* Description */}
+                    <p className="text-left text-sm mt-2 text-gray-700 line-clamp-2">
+                      {article.description}
+                    </p>
+
+                    {/* Published Date */}
+                    <p className="text-left text-xs mt-2 text-gray-500">
+                      {article.published_at}
+                    </p>
+                  </div>
                 </div>
               </Link>
             ))}
           </div>
 
           {/* Right side */}
-          <div className="col-span-1 bg-green-400">
+          <div className="col-span-1 bg-green-400 p-0 w-[420px] ">
             {rightArticles.map((article) => (
               <Link to={`/article/${article.id}`} key={article.id}>
                 {/* Your small news card content */}
                 <div
                   className={`relative py-14 shadow-md hover:shadow-xl
-                rounded-t-xl rounded-b-xl w-full ${
-                  isDarkMode ? "bg-[#101010]" : "bg-white"
-                }`}
-                  /* style={{ maxWidth: "300px" }} */ // Adjust the width as needed
+          rounded-t-xl rounded-b-xl w-full ${
+            isDarkMode ? "bg-[#101010]" : "bg-white"
+          }`}
                 >
                   {/* Small card content */}
                   {/* ... */}
